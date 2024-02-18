84b76c0ba9d676d7501bef24680a52b97acf7b9e
sauki futaki <utaki.sauki19@gmail.com>
2024-02-19 00:33:05 +0700

    Vue compo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
-
 import vercel from "@astrojs/vercel/serverless";
 import vercelServerless from '@astrojs/vercel/serverless';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), vue()],
   output: "server",
-  adapter: vercelServerless(),
+  adapter: vercelServerless()
 });
